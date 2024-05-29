4ffd8b09a0f4cb54bba878716d7212ac6c7c213d
Anurag Verma <104461155+anurag-verma-india@users.noreply.github.com>
2024-05-29 19:10:21 +0530

    Basic strtucture and buttons

---
+++ src/App.1.jsx
@@ -0,0 +1,17 @@
+export function App() {
+  // const [count, setCount] = useState(0)
+  return (
+    <div className="main">
+      <div className="display">1+1=2</div>
+      <div className="buttons">
+        {
+          // let i = 1;
+          // for(i )
+        }
+        // let i = 1; // for(i ) return{" "}
+        <button className="number button">1</button>}
+        <button type="number button">1</button>
+      </div>
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+// import { useState } from 'react'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
+  // const [count, setCount] = useState(0)
+  const button_list = ["C", "+-", "%", "/", 7, 8, 9, "X", 4,5,6,"-",1,2,3,"+",0,".", "="];
+    return (
     <div className="main">
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button><br />
+      <div className="display" id="display">
+        ‍
       </div>
-    </div>
-  )
+      <div className="button_pad">
+          {button_list.map((button, index) => {
+            return (
+              <div key={index} className={button === "=" ? "button_wrapper equals": "button_wrapper"}>
+                <button
+                  onClick={(e) => {
+                    const display_js = document.getElementById("display");
+                    display_js.innerText = e.target.innerText;
+                  }}
+                >
+                  {button}
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+  );
 }
 
-export default App
+export default App;
+
+// <div className="card">
+//   <button onClick={() => setCount((count) => count + 1)}>
+//     count is {count}
+//   </button><br />
+// </div>
